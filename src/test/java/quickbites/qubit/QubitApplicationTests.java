fbec7662c17bc34ee3dad5ad1b419ce349d6f601
gojungsu <gjs4565@naver.com>
2024-12-02 18:31:56 +0900

    feat: 초기 파일

---
+++ src/test/java/quickbites/qubit/QubitApplicationTests.java
@@ -0,0 +1,13 @@
+package quickbites.qubit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QubitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
