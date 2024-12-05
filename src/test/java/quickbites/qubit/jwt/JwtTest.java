810c94df265e537ec7a3c7bbda45dff915c9ad03
gojungsu <gjs4565@naver.com>
2024-12-05 19:45:52 +0900

    feat: jwt Test 클래스

---
+++ src/test/java/quickbites/qubit/jwt/JwtTest.java
@@ -0,0 +1,38 @@
+package quickbites.qubit.jwt;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import quickbites.qubit.domain.User.Role;
+import quickbites.qubit.global.util.jwt.JwtUtil;
+
+import javax.crypto.spec.SecretKeySpec;
+
+import java.time.Duration;
+
+
+public class JwtTest {
+
+    private JwtUtil jwtUtil;
+
+
+    @BeforeEach
+    void setup() {
+        byte[] keyBytes = "GjCU1g8/AbJhSzj4McaCKa7Amu+Fz1N4w8jJfZanEIk=".getBytes(); // 테스트용 키
+
+        jwtUtil = new JwtUtil(
+                new SecretKeySpec(keyBytes, "HmacSHA256"),
+                Duration.ofDays(10000),
+                Duration.ofDays(7),
+                "issuer"
+        );
+    }
+
+    @Test
+    void generateTokenTest(){
+
+        String accessToken = jwtUtil.generateAccessToken("hello", Role.USER);
+        String refreshToken = jwtUtil.generateRefreshToken("hello", Role.USER);
+        System.out.println("token = " + accessToken);
+        System.out.println("refreshToken = " + refreshToken);
+    }
+}
