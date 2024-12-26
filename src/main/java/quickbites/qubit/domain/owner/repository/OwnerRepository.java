986ebc1dd1dd276258e547e54055d118c0f1d824
gojungsu <gjs4565@naver.com>
2024-12-27 04:21:38 +0900

    feat: repository 메소드 추가

--- src/main/java/quickbites/qubit/domain/owner/repository/OwnerRepository.java
+++ src/main/java/quickbites/qubit/domain/owner/repository/OwnerRepository.java
@@ -8,4 +8,6 @@ import java.util.UUID;
 
 public interface OwnerRepository extends JpaRepository<Owner, UUID> {
     Optional<Owner> findByOwnerId(String OwnerId);
+    boolean existsByOwnerId(String ownerId);
+
 }
